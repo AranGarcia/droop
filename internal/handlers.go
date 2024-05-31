0c061e7d35321a564f8118578e2622bbe8e9bf67
Allan Garcia <a.g.jesrez@gmail.com>
2024-05-30 23:59:20 -0600

    implemented endpoints to create character

--- internal/handlers.go
+++ internal/handlers.go
@@ -2,12 +2,40 @@ package internal
 
 import (
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
 
-func postCharacter(_ http.ResponseWriter, _ *http.Request) {
-	log.Println("Creating character")
+type Handler struct {
+	repo *CharacterRepository
+}
+
+func (h Handler) postCharacter(w http.ResponseWriter, r *http.Request) {
+	payload, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Println("failed to read request body bytes")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	character := Character{}
+	if err = json.Unmarshal(payload, &character); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(fmt.Sprintf("failed to unmarshal request payload; %v", err)))
+		return
+	}
+
+	output, err := h.repo.CreateCharacter(r.Context(), character)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	responseBody, err := json.Marshal(output)
+	w.WriteHeader(http.StatusOK)
+	w.Write(responseBody)
+
 }
 
 // getCharacter is a GET endpoint for the Character resource.

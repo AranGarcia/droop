6b376511e0e830ae9a96e3896a436d7b1d79980f
Allan Garcia <a.g.jesrez@gmail.com>
2024-05-30 22:36:23 -0600

    added JSON middleware

--- internal/http.go
+++ internal/http.go
@@ -20,13 +20,21 @@ func NewServer(addr string) *Server {
 }
 
 func (s *Server) Run() error {
-	return http.ListenAndServe(s.addr, loggingMiddleware(s.mux))
+	handler := loggingMiddleware(jsonMiddlewhare(s.mux))
+	return http.ListenAndServe(s.addr, handler)
 }
 
 func loggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s %s", r.Method, r.URL)
 
+		next.ServeHTTP(w, r)
+	})
+}
+
+func jsonMiddlewhare(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
 		next.ServeHTTP(w, r)
 	})
 }

66a4ac8794f079790670bf6a22c475660b00a443
Allan Garcia <a.g.jesrez@gmail.com>
2024-06-10 22:55:22 -0600

    repository: retrieve character entity

--- internal/repository.go
+++ internal/repository.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -16,7 +17,8 @@ const (
 
 // CharacterRepository provides access to the repository where character are stored.
 type CharacterRepository struct {
-	client *mongo.Client
+	client     *mongo.Client
+	collection *mongo.Collection
 }
 
 // NewCharacterRepository initializes a new repository with a MongoDB client.
@@ -27,7 +29,8 @@ func NewCharacterRepository(config MongoConfig) (*CharacterRepository, error) {
 	}
 
 	repo := &CharacterRepository{
-		client: client,
+		client:     client,
+		collection: client.Database(database).Collection(collection),
 	}
 	return repo, nil
 }
@@ -35,11 +38,23 @@ func NewCharacterRepository(config MongoConfig) (*CharacterRepository, error) {
 // CreateCharacter inserts the input character into the database. The same character is returned,
 // but with its ID set.
 func (c CharacterRepository) CreateCharacter(ctx context.Context, character Character) (*Character, error) {
-	coll := c.client.Database(database).Collection(collection)
-	result, err := coll.InsertOne(ctx, character)
+	// Deprecated: use CharacterRepository.collection instead
+	result, err := c.collection.InsertOne(ctx, character)
 	if err != nil {
 		return nil, fmt.Errorf("failed to insert character; %v", err)
 	}
 	character.ID = fmt.Sprint(result.InsertedID)
 	return &character, nil
+}
+
+func (c CharacterRepository) RetrieveCharacter(ctx context.Context, id string) (*Character, error) {
+	result := c.collection.FindOne(ctx, bson.M{"_id": id})
+	if result.Err() != nil {
+		return nil, fmt.Errorf("query error; %v", result.Err())
+	}
+	character := &Character{}
+	if err := result.Decode(character); err != nil {
+		return nil, fmt.Errorf("decoding error; %v", err)
+	}
+	return character, nil
 }

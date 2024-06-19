af518e04de139424b8094fcf1c01cafd63c0d589
Allan Garcia <a.g.jesrez@gmail.com>
2024-06-18 22:56:35 -0600

    empty tests for update

--- characters/internal/ports/repositories/mocks/characters_test.go
+++ characters/internal/ports/repositories/mocks/characters_test.go
@@ -85,3 +85,31 @@ func TestCharacters_Retrieve(t *testing.T) {
 
 	}
 }
+
+func TestCharacters_Update(t *testing.T) {
+	type fields struct {
+		inMemory map[string]entities.Character
+	}
+	type args struct {
+		in0 context.Context
+		in1 entities.Character
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Characters{
+				inMemory: tt.fields.inMemory,
+			}
+			if err := c.Update(tt.args.in0, tt.args.in1); (err != nil) != tt.wantErr {
+				t.Errorf("Characters.Update() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}

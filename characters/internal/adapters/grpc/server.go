c9b94ffa49bbbfd71fa5572182089abe474c5cab
Allan Garcia <a.g.jesrez@gmail.com>
2024-06-18 21:57:56 -0600

    Added grpc adapter

---
+++ characters/internal/adapters/grpc/server.go
@@ -0,0 +1,10 @@
+package grpc
+
+// Server is a gRPC adapter for the character service.
+type Server struct {
+}
+
+// Run the server and start listening for new connections.
+func (s Server) Run() error {
+	return nil
+}

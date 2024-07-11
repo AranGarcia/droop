1d8d25f85576de9e86b6391bb9f526d57971f01d
Allan Garcia <a.g.jesrez@gmail.com>
2024-07-10 21:09:04 -0600

    fix and unit test for repository partial fields

--- characters/internal/adapters/secondary/mongo/update.go
+++ characters/internal/adapters/secondary/mongo/update.go
@@ -9,34 +9,34 @@ import (
 func characterFieldsToBSONMap(fields repositories.CharacterFields) bson.M {
 	m := bson.M{}
 	if fields.Level != nil {
-		m["level"] = fields.Level
+		m["level"] = *fields.Level
 	}
 	if fields.Name != nil {
-		m["name"] = fields.Name
+		m["name"] = *fields.Name
 	}
 	if fields.HealthPoints != nil {
-		m["health_points"] = fields.HealthPoints
+		m["health_points"] = *fields.HealthPoints
 	}
 	if fields.ArmorClass != nil {
-		m["armor_class"] = fields.ArmorClass
+		m["armor_class"] = *fields.ArmorClass
 	}
 	if fields.Strength != nil {
-		m["strength"] = fields.Strength
+		m["strength"] = *fields.Strength
 	}
 	if fields.Dexterity != nil {
-		m["dexterity"] = fields.Dexterity
+		m["dexterity"] = *fields.Dexterity
 	}
 	if fields.Constitution != nil {
-		m["constitution"] = fields.Constitution
+		m["constitution"] = *fields.Constitution
 	}
 	if fields.Intelligence != nil {
-		m["intelligence"] = fields.Intelligence
+		m["intelligence"] = *fields.Intelligence
 	}
 	if fields.Wisdom != nil {
-		m["wisdom"] = fields.Wisdom
+		m["wisdom"] = *fields.Wisdom
 	}
 	if fields.Charisma != nil {
-		m["charisma"] = fields.Charisma
+		m["charisma"] = *fields.Charisma
 	}
 	return m
 }
---
+++ characters/internal/adapters/secondary/mongo/update_test.go
@@ -0,0 +1,75 @@
+package mongo
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/AranGarcia/droop/characters/internal/ports/repositories"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func Test_characterFieldsToBSONMap(t *testing.T) {
+	tests := []struct {
+		name   string
+		fields repositories.CharacterFields
+		want   bson.M
+	}{
+		{
+			name:   "level",
+			fields: repositories.CharacterFields{Level: repositories.IntPtr(1)},
+			want:   bson.M{"level": 1},
+		},
+		{
+			name:   "name",
+			fields: repositories.CharacterFields{Name: repositories.StringPtr("character_name")},
+			want:   bson.M{"name": "character_name"},
+		},
+		{
+			name:   "health_points",
+			fields: repositories.CharacterFields{HealthPoints: repositories.IntPtr(1)},
+			want:   bson.M{"health_points": 1},
+		},
+		{
+			name:   "armor_class",
+			fields: repositories.CharacterFields{ArmorClass: repositories.IntPtr(1)},
+			want:   bson.M{"armor_class": 1},
+		},
+		{
+			name:   "strength",
+			fields: repositories.CharacterFields{Strength: repositories.IntPtr(1)},
+			want:   bson.M{"strength": 1},
+		},
+		{
+			name:   "dexterity",
+			fields: repositories.CharacterFields{Dexterity: repositories.IntPtr(1)},
+			want:   bson.M{"dexterity": 1},
+		},
+		{
+			name:   "constitution",
+			fields: repositories.CharacterFields{Constitution: repositories.IntPtr(1)},
+			want:   bson.M{"constitution": 1},
+		},
+		{
+			name:   "intelligence",
+			fields: repositories.CharacterFields{Intelligence: repositories.IntPtr(1)},
+			want:   bson.M{"intelligence": 1},
+		},
+		{
+			name:   "wisdom",
+			fields: repositories.CharacterFields{Wisdom: repositories.IntPtr(1)},
+			want:   bson.M{"wisdom": 1},
+		},
+		{
+			name:   "charisma",
+			fields: repositories.CharacterFields{Charisma: repositories.IntPtr(1)},
+			want:   bson.M{"charisma": 1},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := characterFieldsToBSONMap(tt.fields); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("characterFieldsToBSONMap() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

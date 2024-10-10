82f8e8abe56d811e70d01094121f2e6bb6715387
Allan Garcia <a.g.jesrez@gmail.com>
2024-10-09 21:24:10 -0600

    added domain events for dnd: rollinitiative

---
+++ dnd/internal/ports/events/events.go
@@ -0,0 +1,18 @@
+// events is an outgoing port that produces domain events.
+package events
+
+import "context"
+
+// Producer of domain events.
+type Producer interface {
+	// RollInitiativeSuccess produces the event of a successful execution of RollInitiative.
+	RollInitiativeSuccess(context.Context, RollInitiativeSuccessMessage) error
+}
+
+// RollInitiativeSuccessMessage contains information on the result of an initiative roll.
+type RollInitiativeSuccessMessage struct {
+	// CharacterID that rolled for initiative.
+	CharacterID string
+	// Result of the roll.
+	Result int
+}

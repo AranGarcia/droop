2fbe2f498fd329773197d179d4665f5f289bf37f
Allan Garcia <a.g.jesrez@gmail.com>
2024-10-23 09:37:26 -0600

    drafted redis repo

---
+++ initiatives/internal/adapters/secondary/redis/tables.go
@@ -0,0 +1,26 @@
+package redis
+
+import (
+	"context"
+
+	"github.com/redis/go-redis/v9"
+
+	"github.com/AranGarcia/initiatives/internal/core/entities"
+)
+
+type TablesRepository struct {
+	client redis.Client
+}
+
+func (t TablesRepository) Create(ctx context.Context, _ entities.Table) error {
+	_ = t.client.SAdd(ctx, "", nil)
+	return nil
+}
+
+func (t TablesRepository) Retrieve(_ context.Context, _ string) (*entities.Table, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (t TablesRepository) Delete(_ context.Context, _ string) error {
+	panic("not implemented") // TODO: Implement
+}

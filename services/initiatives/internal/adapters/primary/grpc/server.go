f217742bae14361ebae69fadcf41cc468150b83c
Allan Garcia <a.g.jesrez@gmail.com>
2024-10-31 23:22:53 -0600

    moved handlers to their own file

--- services/initiatives/internal/adapters/primary/grpc/server.go
+++ services/initiatives/internal/adapters/primary/grpc/server.go
@@ -1,7 +1,6 @@
 package grpc
 
 import (
-	"context"
 	"net"
 
 	"google.golang.org/grpc"
@@ -32,26 +31,4 @@ func (s Server) Run() error {
 	}
 	grpcServer := grpc.NewServer()
 	return grpcServer.Serve(lis)
-}
-
-func (t Server) StartTracking(ctx context.Context, request *initiativespb.StartTrackingRequest) (*initiativespb.StartTrackingResponse, error) {
-	apiRequest := tables.StartTrackingRequest{CampaignID: request.CampaignId}
-	_, err := t.tablesAPI.StartTracking(ctx, apiRequest)
-	if err != nil {
-		// TODO: handle core errors
-		return nil, err
-	}
-	response := &initiativespb.StartTrackingResponse{}
-	return response, nil
-}
-
-func (t Server) StopTracking(ctx context.Context, request *initiativespb.StopTrackingRequest) (*initiativespb.StopTrackingResponse, error) {
-	apiRequest := tables.StopTrackingRequest{CampaignID: request.CampaignId}
-	_, err := t.tablesAPI.StopTracking(ctx, apiRequest)
-	if err != nil {
-		// TODO: handle core errors
-		return nil, err
-	}
-	response := &initiativespb.StopTrackingResponse{}
-	return response, nil
 }

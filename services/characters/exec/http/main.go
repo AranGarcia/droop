70f4e01cd5182b66759c86eb836c08819ee4ec64
Allan Garcia <a.g.jesrez@gmail.com>
2025-02-18 12:37:54 -0600

    http: added support for Access-Control-Allow-Origin

--- services/characters/exec/http/main.go
+++ services/characters/exec/http/main.go
@@ -10,6 +10,8 @@ import (
 var (
 	// addr is the address (HOST:PORT) where the server will run.
 	addr string
+	// accessControlAllowOrigin is used to set response headers for CORS policies.
+	accessControlAllowOrigin string
 	// mongoUser is the username that will connect to the MongoDB.
 	mongoUser string
 	// mongoPassword is the password of the user.
@@ -24,6 +26,7 @@ var (
 
 func init() {
 	flag.StringVar(&addr, "addr", "0.0.0.0:8080", "the server address (host and port)")
+	flag.StringVar(&accessControlAllowOrigin, "access_control_allow_origin", "*", "will be set in response headers for CORS")
 	flag.StringVar(&mongoUser, "mongo_user", "droopadmin", "MongoDB user name")
 	flag.StringVar(&mongoPassword, "mongo_password", "droopadmin", "MongoDB user password")
 	flag.StringVar(&mongoHost, "mongo_host", "localhost", "MongoDB hostname")
@@ -37,7 +40,7 @@ func main() {
 	mongoConfig := buildMongoConfig()
 	repo := buildRepository(mongoConfig)
 	service := buildService(repo)
-	server := http.NewServer(addr, service)
+	server := http.NewServer(addr, accessControlAllowOrigin, service)
 	log.Println("Running server on", addr)
 	if err := server.Run(); err != nil {
 		log.Fatalf("server.Run failure; %v", err)
--- services/characters/internal/adapters/primary/http/server.go
+++ services/characters/internal/adapters/primary/http/server.go
@@ -9,17 +9,22 @@ import (
 
 // Server is an HTTP server.
 type Server struct {
-	addr    string
+	// addr is the address where the server will be running on.
+	addr string
+	// accessControlAllowOrigin is used with CORS policies.
+	accessControlAllowOrigin string
+
 	mux     *http.ServeMux
 	handler *Handler
 }
 
 // NewServer initializes a server with the URI routes added with their handlers.s
-func NewServer(addr string, api api.Characters) *Server {
+func NewServer(addr, accessControlAllowOrigin string, api api.Characters) *Server {
 	s := &Server{
-		addr:    addr,
-		mux:     http.NewServeMux(),
-		handler: NewHandler(api),
+		addr:                     addr,
+		accessControlAllowOrigin: accessControlAllowOrigin,
+		mux:                      http.NewServeMux(),
+		handler:                  NewHandler(api),
 	}
 	s.setupRoutes()
 	return s
@@ -27,15 +32,21 @@ func NewServer(addr string, api api.Characters) *Server {
 
 // Run the server.
 func (s *Server) Run() error {
-	handler := loggingMiddleware(jsonMiddleware(s.mux))
+	handler := loggingMiddleware(jsonMiddleware(s.corsMiddleware(s.mux)))
 	return http.ListenAndServe(s.addr, handler)
+}
+
+func (s Server) corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Access-Control-Allow-Origin", s.accessControlAllowOrigin)
+		next.ServeHTTP(w, r)
+	})
 }
 
 // loggingMiddleware logs all server requests.
 func loggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s %s", r.Method, r.URL)
-
 		next.ServeHTTP(w, r)
 	})
 }

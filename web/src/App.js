390ce376dae8069ba6cfad2f3b0404e23c027ce1
Allan Garcia <a.g.jesrez@gmail.com>
2025-02-18 09:41:26 -0600

    character list screen (experimental)

--- web/src/App.js
+++ web/src/App.js
@@ -1,3 +1,38 @@
+import { useState, useEffect } from 'react';
+
+const Fetch = () => {
+    const [characters, setCharacters] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:8080/characters')
+            .then((res) => {
+                return res.json()
+            })
+            .then((data) => {
+                console.log(data.characters);
+                setCharacters(data.characters);
+            });
+    }, []);
+    console.log("Characters is:", characters);
+    if (characters.length == 0) {
+        return null;
+    }
+
+    return <table>
+        <tr>
+            <th>Name</th>
+            <th>Level</th>
+        </tr>
+        {characters.map(character =>
+            <tr key={character.id}>
+                <td>{character.name}</td>
+                <td>{character.level}</td>
+            </tr>)}
+    </table>
+}
+
 export function App() {
-    return <h1>Hello world!</h1>;
+    return (<div>
+        <h1>Characters</h1>
+        <Fetch />
+    </div>)
 }
